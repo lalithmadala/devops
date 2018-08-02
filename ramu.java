caa0156cc80678accfdec49c523bbbf15f1fa3a8
lalithmadala <lalithmadala95@gmail.com>
2018-08-02 16:49:28 +0000

    text writen

--- ramu.java
+++ ramu.java
@@ -1,3 +1,30 @@
+wehhrvcnmdfgjertyeughFNKJNKJNDF
+IGGIUNIGUERNIVUNRIVIEBVIFDV
+ROJFNERIRBVYVBVUHBVUHBV
+FKLKNDIUVDFUBTSIGFNSDL
+
+public class MammalInt implements Animal {
+
+   public void eat() {
+      System.out.println("Mammal eats");
+   }
+
+   public void travel() {
+      System.out.println("Mammal travels");
+   } 
+
+   public int noOfLegs() {
+      return 0;
+   }
+
+   public static void main(String args[]) {
+      MammalInt m = new MammalInt();
+      m.eat();
+      m.travel();
+   }
+} 
+
+ 
 public class HelloWorld {
 
    public static void main(String[] args) {
