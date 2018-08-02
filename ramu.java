53f140aecf7f9f10d77dda1a52ef7dadd92e804f
lalithmadala <lalithmadala95@gmail.com>
2018-08-02 16:33:01 +0000

    codde will added

--- ramu.java
+++ ramu.java
@@ -1,4 +1,13 @@
-public class className {
+public class HelloWorld {
+
+   public static void main(String[] args) {
+      /* Prints Hello, World! on standard output.
+      System.out.println("Hello World!");
+   }
+}
+
+
+ public class className {
    // ...
 }
 
