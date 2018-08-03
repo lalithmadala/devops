931a9d67d535abeaac9a9d43186a7a45964acea3
lalithmadala <lalithmadala95@gmail.com>
2018-08-03 07:32:23 +0000

    java repo

--- demo.java
+++ demo.java
@@ -1,10 +1,25 @@
-ai hhsjdhsijdkdfmdfimf
-n
-nnvkmfkvvdfvgg
-hgffgfdgdgddfffffffffffffffffffffffffffffffffffffffff
+public class MammalInt implements Animal {
 
-sdewefrerffff
-f
+   public void eat() {
+      System.out.println("Mammal eats");
+   }
+
+   public void travel() {
+      System.out.println("Mammal travels");
+   } 
+
+   public int noOfLegs() {
+      return 0;
+   }
+
+   public static void main(String args[]) {
+      MammalInt m = new MammalInt();
+      m.eat();
+      m.travel();
+   }
+} 
+
+
 
 public class CallingMethodsInSameClass
 {
